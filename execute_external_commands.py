a38747d953cd3600cc6774e01aa889cf0088b5d8
mkrhca <mkrhca@gmail.com>
2020-04-13 13:56:10 +0530

    Create execute_external_commands.py

---
+++ execute_external_commands.py
@@ -0,0 +1,8 @@
+import subprocess
+subprocess.call(["ls"])
+subprocess.call(["touch", "sample.txt"])
+subprocess.call(["ls"])
+print("Sample file created")
+subprocess.call(["rm", "sample.txt"])
+subprocess.call(["ls"])
+print("Sample file deleted")
